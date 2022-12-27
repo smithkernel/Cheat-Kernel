be5940cc46a6ad1957da89a37b1a9e44695e75b9
Slackes <SlackesCC@pm.me>
2022-12-28 02:51:59 +0700

    Update auto-changelog.sh

--- scripts/auto-changelog.sh
+++ scripts/auto-changelog.sh
@@ -6,55 +6,56 @@ set -o pipefail
 
 readonly URL='https://github.com/cachyos/kernel-manager'
 
+# Fetch all tags from the remote repository
 git fetch --tags
 
+# Get the most recent tag
 most_recent_tag="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
 
-git rev-list "${most_recent_tag}"..HEAD | while read -r commit
-do
+# Get a list of all commits made after the most recent tag
+commits="$(git rev-list "${most_recent_tag}"..HEAD)"
+
+# Check if there are any commits to process
+if [[ -z "$commits" ]]; then
+    echo "No new commits found."
+    exit 0
+fi
+
+# Iterate over the commits
+while read -r commit; do
+    # Get the commit subject
     subject="$(git show -s --format="%s" "$commit")"
 
-    # Squash & Merge: the commit subject is parsed as `<description> (#<PR ID>)`
-    if grep -E -q '\(#[[:digit:]]+\)' <<< "$subject"
-    then
+    # Determine the type of commit and extract the necessary information
+    if grep -E -q '\(#[[:digit:]]+\)' <<< "$subject"; then
+        # Squash & Merge commit
         pr="$(awk '{print $NF}' <<< "$subject" | tr -d '()')"
         prefix="[$pr]($URL/pull/${pr###}): "
         description="$(awk '{NF--; print $0}' <<< "$subject")"
-
-    # Merge: the PR ID is parsed from the git subject (which is of the form `Merge pull request
-    #   #<PR ID> from <branch>`, and the description is assumed to be the first line of the body.
-    #   If no body is found, the description is set to the commit subject
-    elif grep -E -q '#[[:digit:]]+\sfrom' <<< "$subject"
-    then
+    elif grep -E -q '#[[:digit:]]+\sfrom' <<< "$subject"; then
+        # Merge commit
         pr="$(awk '{print $4}' <<< "$subject")"
         prefix="[$pr]($URL/pull/${pr###}): "
 
+        # Get the first line of the commit body as the description
         first_line_of_body="$(git show -s --format="%b" "$commit" | head -n 1 | tr -d '\r')"
-        if [[ -z "$first_line_of_body" ]]
-        then
+        if [[ -z "$first_line_of_body" ]]; then
+            # Use the subject as the description if there is no body
             description="$subject"
         else
             description="$first_line_of_body"
         fi
-
-    # Normal commits: The commit subject is the description, and the PR ID is omitted.
     else
+        # Normal commit
         pr=''
         prefix=''
         description="$subject"
     fi
 
-    # add entry to CHANGELOG
-    if [[ "$OSTYPE" == "linux-gnu" ]]
-    then
-        # shellcheck disable=SC1004
-        sed -i'' '/## Current Develop Branch/a\
+    # Add an entry to the CHANGELOG for the commit
+    sed -i'' '/## Current Develop Branch/a\
 - '"$prefix$description"''$'\n' CHANGELOG.md
-    else
-        # shellcheck disable=SC1004
-        sed -i '' '/## Current Develop Branch/a\
-- '"$prefix$description"''$'\n' CHANGELOG.md
-    fi
-done
+done <<< "$commits"
 
 echo 'CHANGELOG updated'
+
